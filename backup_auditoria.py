32fa11ab7e76b22e906527cfd7ab2cc554db7139
tassanf@gmail.com <#git2462>
2016-08-31 13:25:03 -0300

    Busca de arquivos no diretório usando glob

--- backup_auditoria.py
+++ backup_auditoria.py
@@ -1,9 +1,8 @@
-from os import walk
+import glob
 from os import getcwd
 
 dir_path = getcwd()
 
-for root, dirs, files in walk(dir_path):
-    for file in files:
-        if file.endswith(".txt"):
-            print(file)
+for files in glob.glob1(dir_path, '*.txt'):
+    print(files)
+
